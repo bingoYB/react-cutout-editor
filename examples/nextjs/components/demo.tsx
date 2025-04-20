e7028a72bd462b803f3c0d7d258242ea2b21da01
bingoYB <binhdu@foxmail.com>
2025-04-20 22:11:19 +0800

    Update demo.tsx

--- examples/nextjs/components/demo.tsx
+++ examples/nextjs/components/demo.tsx
@@ -3,7 +3,7 @@ import CutoutEditor from 'react-cutout-editor';
 export default function Demo() {
   return (
     <div className="h-screen">
-      <CutoutEditor maskImgUrl="/maskdemo.png" imgUrl="/test.webp" />
+      <CutoutEditor maskImgUrl="https://bingoyb.github.io/react-cutout-editor/nextjs//maskdemo.png" imgUrl="https://bingoyb.github.io/react-cutout-editor/nextjs/test.webp" />
     </div>
   );
 }
