4d7e5b6ab356a0972f333775c1bb0372fa8f9b1c
bingoYB <binhdu@foxmail.com>
2025-04-20 22:04:31 +0800

    fix: adjust  base url

--- examples/nextjs/next.config.ts
+++ examples/nextjs/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   distDir: 'dist',
   basePath: process.env.NODE_ENV === 'production' ? '/react-cutout-editor/nextjs' : '/react-cutout-editor/examples/nextjs',
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://bingoyb.github.io/custom-demo/nextjs' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://bingoyb.github.io/react-cutout-editor/nextjs' : undefined,
 };
 
 export default nextConfig;
