da89dbf1c2062928edc368368040688fe13c1158
bingoYB <binhdu@foxmail.com>
2025-04-20 21:46:21 +0800

    Update next.config.ts

--- examples/nextjs/next.config.ts
+++ examples/nextjs/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   distDir: 'dist',
-  basePath: process.env.NODE_ENV === 'production' ? '/react-cutout-editor/nextjs/' : '/react-cutout-editor/examples/nextjs',
+  basePath: process.env.NODE_ENV === 'production' ? '/react-cutout-editor/nextjs' : '/react-cutout-editor/examples/nextjs',
   output: 'export',
 };
 
